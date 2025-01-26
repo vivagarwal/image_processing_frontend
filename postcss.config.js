e5e274d233f8c6c28b7ab416bc033c31bd28ee2c
vivagarwal <vivek.jsr.topper@gmail.com>
2025-01-27 03:22:27 +0530

    initial commit

---
+++ postcss.config.js
@@ -0,0 +1,7 @@
+export default {
+    plugins: {
+      '@tailwindcss/postcss': {}, // Use the correct Tailwind PostCSS plugin
+      autoprefixer: {},
+    }
+  };
+  
---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { BrowserRouter, Routes, Route , Navigate} from "react-router-dom";
+import './index.css';
+import HelloPage from "./pages/HelloPage";
+
+const App = () => {
+  return (
+      <div className="container mt-4">
+        <Routes>
+        <Route path="/" element={<HelloPage />} />
+        </Routes>
+    </div>
+  );
+};
+
+const AppWrapper = () => {
+  return (
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  );
+};
+
+export default AppWrapper;
